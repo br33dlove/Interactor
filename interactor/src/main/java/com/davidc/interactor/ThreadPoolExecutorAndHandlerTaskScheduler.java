10e73dd5ab3da42ee0e782e1045461f99eb58ce7
David Cryer <davidc@apadmi.com>
2017-03-29 10:53:31 +0100

    add class

---
+++ interactor/src/main/java/com/davidc/interactor/ThreadPoolExecutorAndHandlerTaskScheduler.java
@@ -0,0 +1,25 @@
+package com.davidc.interactor;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPoolExecutorAndHandlerTaskScheduler extends ThreadPoolExecutorTaskScheduler {
+    private final Handler handler;
+
+    public ThreadPoolExecutorAndHandlerTaskScheduler(ThreadPoolExecutor threadPoolExecutor, Looper looper) {
+        super(threadPoolExecutor);
+        this.handler = new Handler(looper);
+    }
+
+    @Override
+    public void executeOnBoundThread(final Task task) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.execute();
+            }
+        });
+    }
+}
